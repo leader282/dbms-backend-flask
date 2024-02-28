8779f0f125a76c51e98b3f6d5d8e3b993f193694
somya-kr <somyakumar958@gmail.com>
2024-02-28 19:07:27 +0530

    Refactor admin_event route to fetch all events

--- routers/admin/event.py
+++ routers/admin/event.py
@@ -11,25 +11,25 @@ admin_event = Blueprint('admin_event', __name__)
 
 config  = load_config()
 
-# @admin_event.route('/events', methods=['GET'])
-# @jwt_required()
-# def all_events():
-#     user_details = get_jwt_header()
-#     if(user_details['role'] != 'admin'):
-#         return jsonify({'message': 'Unauthorized'}), 401
-#     try:
-#         with psycopg2.connect(**config) as conn:
-#             with conn.cursor() as cur:
-#                 # Executing the selected query
-#                 cur.execute(f"SELECT * FROM EVENT;")
-#                 rows = cur.fetchall()
-#                 if not rows:
-#                     return jsonify({'details': ''}), 200
-#                 else:
-#                     return jsonify({'data': rows}), 200
-#     except (Exception, psycopg2.DatabaseError) as error:
-#         print(error)
-#         return jsonify({'message': 'Error Fetching events'}), 404
+@admin_event.route('/events', methods=['GET'])
+@jwt_required()
+def all_events():
+    user_details = get_jwt_header()
+    if(user_details['role'] != 'admin'):
+        return jsonify({'message': 'Unauthorized'}), 401
+    try:
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+                # Executing the selected query
+                cur.execute(f"SELECT * FROM EVENT;")
+                rows = cur.fetchall()
+                if not rows:
+                    return jsonify({'details': ''}), 200
+                else:
+                    return jsonify({'data': rows}), 200
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return jsonify({'message': 'Error Fetching events'}), 404
 
 @admin_event.route('/add_event', methods=['POST'])
 @jwt_required()
